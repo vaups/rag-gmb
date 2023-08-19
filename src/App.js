8fecf0ae7a4297144b88f819f842ae4539c45371
Jacob Vaupell <vaupjr@proton.me>
2023-08-19 11:05:36 -0500

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { Select, Card, Avatar, Rate, Typography, Button } from 'antd';
+import { Select, Card, Avatar, Rate, Typography, Button, Spin } from 'antd';
 import './App.css';
 
 const { Option } = Select;
 const { Title, Text } = Typography;
+
+function Reviews({ reviews }) {
+    return (
+        <div className="reviews-container">
+            {reviews.map(review => (
+                <Card key={review.reviewId} style={{ marginTop: 16 }}>
+                    <Card.Meta
+                        avatar={<Avatar src={review.reviewer.profilePhotoUrl} />}
+                        title={review.reviewer.displayName}
+                        description={review.comment}
+                    />
+                    <Rate disabled value={parseInt(review.starRating, 10)} />
+                    <Text type="secondary">{new Date(review.createTime).toLocaleDateString()}</Text>
+                </Card>
+            ))}
+        </div>
+    );
+}
 
 function App() {
     const [locations, setLocations] = useState([]);
     const [selectedLocation, setSelectedLocation] = useState('');
     const [reviews, setReviews] = useState([]);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const API_URL = 'https://rag-gmb-73f4cd98333b.herokuapp.com';
 
@@ -19,6 +38,11 @@ function App() {
             .then(response => response.json())
             .then(data => {
                 setIsAuthenticated(data.isAuthenticated);
+                setLoading(false);
+            })
+            .catch(() => {
+                setLoading(false);
+                alert('Error checking authentication status.');
             });
 
         // Fetch locations from Flask app
@@ -26,18 +50,31 @@ function App() {
             .then(response => response.json())
             .then(data => {
                 setLocations(data.locations);
+            })
+            .catch(() => {
+                alert('Error fetching locations.');
             });
     }, []);
 
     useEffect(() => {
         if (selectedLocation) {
+            setLoading(true);
             fetch(`${API_URL}/getReviews/${selectedLocation}`)
                 .then(response => response.json())
                 .then(data => {
                     setReviews(data.reviews);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    setLoading(false);
+                    alert('Error fetching reviews.');
                 });
         }
     }, [selectedLocation]);
+
+    if (loading) {
+        return <Spin size="large" />;
+    }
 
     return (
         <div className="app-container">
@@ -58,19 +95,7 @@ function App() {
                             <Option key={location} value={location}>{location}</Option>
                         ))}
                     </Select>
-                    <div className="reviews-container">
-                        {reviews.map(review => (
-                            <Card key={review.reviewId} style={{ marginTop: 16 }}>
-                                <Card.Meta
-                                    avatar={<Avatar src={review.reviewer.profilePhotoUrl} />}
-                                    title={review.reviewer.displayName}
-                                    description={review.comment}
-                                />
-                                <Rate disabled value={parseInt(review.starRating, 10)} />
-                                <Text type="secondary">{new Date(review.createTime).toLocaleDateString()}</Text>
-                            </Card>
-                        ))}
-                    </div>
+                    <Reviews reviews={reviews} />
                 </>
             )}
         </div>
