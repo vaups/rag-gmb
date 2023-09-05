508213c8e2213d7eec4b96a33b77b8b07e68a1bb
Jacob Vaupell <vaupjr@proton.me>
2023-09-05 12:31:43 -0500

    V2

--- src/App.js
+++ src/App.js
@@ -11,27 +11,28 @@ function App() {
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  // Constants
-  const LOCATIONS = {
-    "Reed Jeep Chrysler Dodge Ram of Kansas City Service Center": ("107525660123223074874", "6602925040958900944"),
-    "Reed Jeep of Kansas City": ("107525660123223074874", "1509419292313302599"),
-    "Reed Jeep Chrysler Dodge Ram of Kansas City Parts Store": ("107525660123223074874", "13301160076946238237"),
-    "Reed Chrysler Dodge Jeep Ram": ("111693813506330378362", "11797626926263627465"),
-    "Reed Jeep Ram Service Center of St. Joseph": ("111693813506330378362", "14280468831929260325"),
-    "Reed Jeep Ram Parts Store": ("111693813506330378362", "2418643850076402830"),
-    "Reed Hyundai St. Joseph": ("106236436844097816145", "11886236645408970450"),
-    "Reed Hyundai Service Center St. Joseph": ("106236436844097816145", "14394473597121013675"),
-    "Reed Hyundai of Kansas City": ("109745744288166151974", "8949845982319380160"),
-    "Reed Hyundai Service Center of Kansas City": ("109745744288166151974", "14191266722711425624"),
-    "Reed Hyundai Parts Store": ("109745744288166151974", "16832194732739486696"),
-    "Reed Collision Center": ("118020935772003776996", "14476819248161239911"),
-    "Reed Chevrolet of St Joseph": ("101540168465155832676", "4906344306812977154"),
-    "Reed Chevrolet Service Center": ("101540168465155832676", "7432353734414121407"),
-    "Reed Chevrolet Parts": ("101540168465155832676", "13264330561216148213"),
-    "Reed Buick GMC, INC.": ("109231983509135903650", "9980434767027047433"),
-    "Reed Buick GMC Service Center": ("109231983509135903650", "9597638825461585665"),
-    "Reed Buick GMC Collision Center": ("109231983509135903650", "10315051056232587965")
+// Constants
+const LOCATIONS = {
+  "Reed Jeep Chrysler Dodge Ram of Kansas City Service Center": ["107525660123223074874", "6602925040958900944"],
+  "Reed Jeep of Kansas City": ["107525660123223074874", "1509419292313302599"],
+  "Reed Jeep Chrysler Dodge Ram of Kansas City Parts Store": ["107525660123223074874", "13301160076946238237"],
+  "Reed Chrysler Dodge Jeep Ram": ["111693813506330378362", "11797626926263627465"],
+  "Reed Jeep Ram Service Center of St. Joseph": ["111693813506330378362", "14280468831929260325"],
+  "Reed Jeep Ram Parts Store": ["111693813506330378362", "2418643850076402830"],
+  "Reed Hyundai St. Joseph": ["106236436844097816145", "11886236645408970450"],
+  "Reed Hyundai Service Center St. Joseph": ["106236436844097816145", "14394473597121013675"],
+  "Reed Hyundai of Kansas City": ["109745744288166151974", "8949845982319380160"],
+  "Reed Hyundai Service Center of Kansas City": ["109745744288166151974", "14191266722711425624"],
+  "Reed Hyundai Parts Store": ["109745744288166151974", "16832194732739486696"],
+  "Reed Collision Center": ["118020935772003776996", "14476819248161239911"],
+  "Reed Chevrolet of St Joseph": ["101540168465155832676", "4906344306812977154"],
+  "Reed Chevrolet Service Center": ["101540168465155832676", "7432353734414121407"],
+  "Reed Chevrolet Parts": ["101540168465155832676", "13264330561216148213"],
+  "Reed Buick GMC, INC.": ["109231983509135903650", "9980434767027047433"],
+  "Reed Buick GMC Service Center": ["109231983509135903650", "9597638825461585665"],
+  "Reed Buick GMC Collision Center": ["109231983509135903650", "10315051056232587965"]
 };
+
 
 // Fetch reviews when a location is selected
 useEffect(() => {
